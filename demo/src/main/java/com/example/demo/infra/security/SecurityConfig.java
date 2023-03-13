f1297bd67dd1a2a0c88d0de0d479636a2d836538
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2023-03-13 21:49:13 +0900

    [개발] e-commerce msa 프로젝트를 위한 설정

---
+++ demo/src/main/java/com/example/demo/infra/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.infra.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+    }
+}
