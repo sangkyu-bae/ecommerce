4906180809d5eb012629f3ec7935bb6c119b6533
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2024-01-29 22:33:53 +0900

    [개발] redis 분산락 구현

--- coupon/src/main/java/org/example/coupon/infra/redis/DistributedLock.java
+++ coupon/src/main/java/org/example/coupon/infra/redis/DistributedLock.java
@@ -1,29 +1,37 @@
 package org.example.coupon.infra.redis;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.concurrent.TimeUnit;
 
-public interface DistributedLock {
+/**
+ * Redisson Distributed Lock annotation
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DistributedLock {
+
+    /**
+     * 락의 이름
+     */
+    String key();
 
     /**
      * 락의 시간 단위
      */
-    default TimeUnit timeUnit() {
-        return TimeUnit.SECONDS;
-    }
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
 
     /**
      * 락을 기다리는 시간 (default - 5s)
      * 락 획득을 위해 waitTime 만큼 대기한다
      */
-    default long waitTime() {
-        return 5L;
-    }
+    long waitTime() default 5L;
 
     /**
      * 락 임대 시간 (default - 3s)
      * 락을 획득한 이후 leaseTime 이 지나면 락을 해제한다
      */
-    default long leaseTime() {
-        return 3L;
-    }
+    long leaseTime() default 3L;
 }
--- coupon/src/main/java/org/example/coupon/infra/redis/RedisService.java
+++ coupon/src/main/java/org/example/coupon/infra/redis/RedisService.java
@@ -13,22 +13,8 @@ import org.springframework.stereotype.Service;
 public class RedisService {
     private final RedissonClient redissonClient;
 
-    private final DistributedLock distributedLock;
 
     public void lock(String key){
-        RLock lock = redissonClient.getLock(key);
-        try {
-            boolean available = lock.tryLock(distributedLock.waitTime(), distributedLock.leaseTime(), distributedLock.timeUnit());
 
-            if (!available) {
-                //Lock 획득 실패
-                return;
-            }
-
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            lock.unlock();
-        }
     }
 }
