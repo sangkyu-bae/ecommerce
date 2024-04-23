f509d63afa25bd3c7ab20a644e682a668cf2000a
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2024-04-23 22:59:37 +0900

    [수정] 구매 리팩토링

--- customer-view/components/product/ProductSizeQuantity.tsx
+++ customer-view/components/product/ProductSizeQuantity.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
@@ -9,25 +9,60 @@ import {useProductActionContext, useProductValueContext} from "@/components/prod
 function ProductSizeQuantity(props) {
     const {productData} = useProductValueContext();
     const actions = useProductActionContext();
+    const [colorValue,setColorValue] = useState <number>(0);
+    const [sizeValue, setSizeValue] = useState<number> (0);
+
+
+    const onChangeColorValue = (value : number) =>{
+        if(value == 0){
+            setSizeValue(0);
+        }
+        setColorValue(value);
+    }
+    const sizeData = productData.productComponents
+        .find(data => data.color.id == colorValue)
+        ?.sizes || [];
+
+
     return (
         <Box sx={{ minWidth: 120 ,mt:2}}>
             <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Size</InputLabel>
+                <InputLabel id="color">Color</InputLabel>
                 <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    // value={basket.productSizeId}
-                    label="Size"
-                    onChange={e=>actions.change(e)}
-                    name = "productSizeId"
+                    labelId="color-select"
+                    id="color-select"
+                    value={colorValue}
+                    label="color"
+                    onChange={e=>{
+                        onChangeColorValue(e.target.value)
+                    }}
+                    name = "color"
                 >
+                    <MenuItem value = {0}>옵션 선택</MenuItem>
                     {
-                        productData.productComponents.map(component => (
-                            component.sizes.map(size => (
-                                <MenuItem key={size.id} value={size.id}>color: {component.color.name},size: {size.size}, quantity: {size.quantity} </MenuItem>
-                            ))
-                        ))
+                        productData.productComponents
+                            .map(component => <MenuItem key ={component.color.id} value = {component.color.id}>{component.color.name}</MenuItem> )
                     }
+                </Select>
+
+                <InputLabel id="size-select">Size</InputLabel>
+                <Select
+                    sx={{mt:5}}
+                    labelId="size-select"
+                    id="size-select"
+                    value={sizeValue}
+                    label="Size"
+                    onChange={e=>{
+                        setSizeValue(e.target.value)
+                    }}
+                    name = "size"
+                >
+                    <MenuItem value = {0}>옵션 선택</MenuItem>
+                    {
+                        sizeData
+                            .map(sizeComponent => <MenuItem key ={sizeComponent.id} value = {sizeComponent.id}>size : {sizeComponent.size} / quantity : {sizeComponent.quantity}</MenuItem> )
+                    }
+
                 </Select>
             </FormControl>
         </Box>
