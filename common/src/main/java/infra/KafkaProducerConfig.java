06694b1dc33f11144977dfa410b7d1fe2d300417
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2024-08-28 22:47:40 +0900

    [이슈] common 모듈 kafka 프로듀서 config 이슈

--- common/src/main/java/infra/KafkaProducerConfig.java
+++ common/src/main/java/infra/KafkaProducerConfig.java
@@ -1,7 +1,10 @@
 package infra;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,28 +12,65 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
-@Configuration
-@RequiredArgsConstructor
+@Component
+//@RequiredArgsConstructor
+@Slf4j
 public class KafkaProducerConfig {
 
-    @Value("${app.bootstrapServer}")
-    String bootstrapServer;
-    @Bean
-    public ProducerFactory<String,String> producerFactory(){
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//    @Value("${app.common.bootstrap.server}")
+//    String commonBootstrapServer;
+//    @Bean
+//    public ProducerFactory<String,String> producerFactory(){
+//        Map<String, Object> configProps = new HashMap<>();
+//        log.info("=============================");
+//        log.info("bootstarpServer : " + commonBootstrapServer);
+//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"sakjdfljdsfjlsdfhsfdj");
+//        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//
+//        return new DefaultKafkaProducerFactory<>(configProps);
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String,String> kafkaTemplate(){
+//        return new KafkaTemplate<>(producerFactory());
+////        return null;
+//    }
+    private final KafkaProducer<String, String> producer;
+    private final String topic;
+    public KafkaProducerConfig() {
 
-        return new DefaultKafkaProducerFactory<>(configProps);
+
+        // Producer Initialization ';'
+        Properties props = new Properties();
+
+        // kafka:29092
+        props.put("bootstrap.servers", "!@34536");
+
+        // "key:value"
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        this.producer = new KafkaProducer<>(props);
+        this.topic = "sdkjsdfkj";
     }
 
-    @Bean
-    public KafkaTemplate<String,String> kafkaTemplate(){
-        return new KafkaTemplate<>(producerFactory());
+    // Kafka Cluster [key, value] Produce
+    public void sendMessage(String key, String value) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+        producer.send(record, (metadata, exception) -> {
+            if (exception == null) {
+                // System.out.println("Message sent successfully. Offset: " + metadata.offset());
+            } else {
+                exception.printStackTrace();
+                // System.err.println("Failed to send message: " + exception.getMessage());
+            }
+        });
     }
 }
--- common/src/main/java/org/example/kafka/NotificationProducer.java
+++ common/src/main/java/org/example/kafka/NotificationProducer.java
@@ -2,32 +2,75 @@ package org.example.kafka;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import infra.KafkaProducerConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.example.aop.NotificationClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.Properties;
+
 @Component
 @Slf4j
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class NotificationProducer {
 
-    private final KafkaTemplate<String,String> kafkaTemplate;
+    private final KafkaProducer<String, String> producer;
 
     private final ObjectMapper objectMapper;
 
-    @Value("${kafka.notification.topic}")
-    private String SEND_NOTIFICATION_TOPIC;
+//    @Value("${kafka.notification.topic}")
+//    private String SEND_NOTIFICATION_TOPIC;
+//    @Value("${app.common.bootstrap.server}")
+//    String commonBootstrapServer;
 
+
+//    private final KafkaProducerConfig kafkaProducerConfig;
+    private final String topic;
+
+
+
+    public NotificationProducer(){
+        this.topic = "test";
+        Properties props = new Properties();
+
+        // kafka:29092
+        props.put("bootstrap.servers", "Test");
+
+        // "key:value"
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        this.producer = new KafkaProducer<>(props);
+        objectMapper = new ObjectMapper();
+    }
     public void sendCreateNotification(NotificationClient notificationClient){
         try{
             String inputJsonString = objectMapper.writeValueAsString(notificationClient);
             log.info(inputJsonString);
-            kafkaTemplate.send(SEND_NOTIFICATION_TOPIC,inputJsonString);
+            log.error("============info============");
+//            log.error("log topic : {}" ,SEND_NOTIFICATION_TOPIC);
+//            log.error("log bootstrap : {}" ,commonBootstrapServer);
+            log.error("============info============");
+//            kafkaTemplate.send(SEND_NOTIFICATION_TOPIC,inputJsonString);
+//            kafkaProducerConfig.sendMessage(SEND_NOTIFICATION_TOPIC,inputJsonString);
+
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, "test","Test");
+            producer.send(record, (metadata, exception) -> {
+                if (exception == null) {
+                    // System.out.println("Message sent successfully. Offset: " + metadata.offset());
+                } else {
+                    exception.printStackTrace();
+                    // System.err.println("Failed to send message: " + exception.getMessage());
+                }
+            });
         }catch (JsonProcessingException e){
             log.error("fail send notification service : {}" , e);
         }
     }
+
+
 }
