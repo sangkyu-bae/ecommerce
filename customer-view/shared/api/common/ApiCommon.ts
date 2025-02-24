9287b60f7a9eae851fd8add494d2bd438e5fee20
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2025-02-24 21:59:31 +0900

    jwt token 접근 오류 수정

--- customer-view/shared/api/common/ApiCommon.ts
+++ customer-view/shared/api/common/ApiCommon.ts
@@ -1,28 +1,37 @@
 import axios from "axios";
-import {getAccessToken} from "@/shared/api/cookie/Cookie";
-let accessToken = getAccessToken()
+import { getAccessToken } from "@/shared/api/cookie/Cookie";
+// let accessToken = getAccessToken()
 
-const ApiCommon={
-    basicAPI:axios.create({
-        baseURL:"/",
+const ApiCommon = {
+    basicAPI: axios.create({
+        baseURL: "/",
         headers: {
             'Content-Type': 'application/json'
         }
     }),
-    loginAPI:axios.create({
-        baseURL:"/",
+    loginAPI: axios.create({
+        baseURL: "/",
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Authorization:`${accessToken}`
+            'Content-Type': 'application/x-www-form-urlencoded'
         }
     }),
-    loginJsonAPI:axios.create({
-        baseURL:"/",
+    loginJsonAPI: axios.create({
+        baseURL: "/",
         headers: {
-            'Content-Type': 'application/json',
-            Authorization:`${accessToken}`
+            'Content-Type': 'application/json'
         }
-    }),
-}
+    })
+};
+
+// 인증이 필요한 API에 인터셉터 추가
+[ApiCommon.loginAPI, ApiCommon.loginJsonAPI].forEach(instance => {
+    instance.interceptors.request.use(config => {
+        const token = getAccessToken();
+        if (token) {
+            config.headers.Authorization = token;
+        }
+        return config;
+    });
+});
 
 export default ApiCommon;
